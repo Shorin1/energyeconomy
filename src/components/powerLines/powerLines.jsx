1b098595c0dd02833b96421b8aa7351940857217
Дмитрий <patrick_lz@live.com>
2021-02-15 20:49:53 +0400

    Минус задача из первого раздела

--- src/components/powerLines/powerLines.jsx
+++ src/components/powerLines/powerLines.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Theory from './theory';
-import Example1 from './example1';
 
 const PowerLines = () => {
     return (
         <>
             <Theory />
-            <Example1 />
         </>
     );
 }
